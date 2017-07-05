919e388f142226ad64ca12323302c7091635dfa6
jerry <jerry@192.168.0.103>
2017-07-05 21:21:34 +0800

    food菜单flyway

---
+++ src/main/resources/flyway/V2017_0705_2122__INSERT_FOOD_MENU.sql
@@ -0,0 +1,2 @@
+insert into t_menu values('07','0','food','商品','fa fa-cutlery','','2017-07-03 23:21:36');
+insert into t_menu values('0701','07','','商品列表','fa fa-list-ol','/food','2017-07-03 23:21:36');
