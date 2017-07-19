4dcb8a3a9ab4b4eb1eabaab2d02703ecf022f7ba
jerry <jerry@192.168.0.103>
2017-07-19 23:03:46 +0800

    商品添加

--- src/main/java/core/admin/controller/food/FoodController.java
+++ src/main/java/core/admin/controller/food/FoodController.java
@@ -1,21 +1,29 @@
 package core.admin.controller.food;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.h2.util.New;
+import org.apache.commons.lang3.StringUtils;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.jfinal.core.Controller;
 import com.jfinal.log.Log;
 import com.jfinal.plugin.activerecord.Page;
 import com.jfinal.plugin.activerecord.Record;
+import com.jfinal.upload.UploadFile;
 
 import core.admin.service.food.FoodService;
 import core.admin.service.food.impl.FoodServiceImpl;
+import core.common.utils.QiniuUtils;
 import core.model.Food;
+import core.model.FoodType;
+import core.model.Shop;
 import core.vo.DTParams;
+import core.vo.JSONError;
+import core.vo.JSONSuccess;
 
 /**
  * 
@@ -52,7 +60,55 @@ public class FoodController extends Controller {
 	}
 
 	public void save() {
-		renderText("123");
+		try {
+			Food food = new Food();
+			try {
+				UploadFile file = getFile("IMG");
+				if (file != null) {
+					String localFilePath = file.getUploadPath() + File.separator + file.getFileName();
+					String path = QiniuUtils.upload(localFilePath);
+					String crop = getPara("crop");// 截图参数
+					food.set("IMG", path + (StringUtils.isNotEmpty(crop) ? crop : ""));
+				}
+			} catch (Exception e) {
+				renderJson(new JSONError("图片错误"));
+				return;
+			}
+			String shopName = getPara("shopName");
+			Shop shop = foodService.findByShopName(shopName);
+			if (shop == null) {
+				renderJson(new JSONError("店铺不存在"));
+				return;
+			}
+			String name = getPara("NAME");
+			Food food1 = foodService.findByName(name);
+			if (food1 != null) {
+				renderJson(new JSONError("商品已存在"));
+				return;
+			}
+			String price = getPara("price");
+			String UNIT = getPara("UNIT");
+			String DESCRIPTION = getPara("DESCRIPTION");
+			Boolean USE_STOCK = getParaToBoolean("USE_STOCK");
+			String STOCK = getPara("STOCK");
+			food.set("SHOP_ID", shop.get("ID"));
+			food.set("NAME", name);
+			food.set("PRICE", price);
+			food.set("UNIT", UNIT);
+			food.set("DESCRIPTION", DESCRIPTION);
+			food.set("USE_STOCK", USE_STOCK);
+			if (USE_STOCK != null && USE_STOCK)
+				food.set("STOCK", STOCK);
+			else
+				food.set("STOCK", 999999);
+			food.set("STATE", 1);
+			food.save();
+			renderJson(new JSONSuccess("添加成功"));
+		} catch (Exception e) {
+			e.printStackTrace();
+			renderJson(new JSONError("1"));
+
+		}
 	}
 
 	public void getHistroyData() {
@@ -74,10 +130,40 @@ public class FoodController extends Controller {
 
 	public void checkShopName() {
 		String shopName = getPara("shopName");
-		if (foodService.checkShopName(shopName))
-			renderText("验证通过");
-		else
-			renderText("不存在商家");
+		JSONObject resultObject = new JSONObject();
+		JSONArray typeArray = new JSONArray();
+		if (foodService.findByShopName(shopName) != null) {
+			resultObject.put("mess", "存在商家");
+			List<FoodType> foodTypes = foodService.findFoodTypeByShopName(shopName);
+			for (FoodType foodType : foodTypes) {
+				JSONObject typeObject = new JSONObject();
+				typeObject.put("id", foodType.get("ID"));
+				typeObject.put("name", foodType.get("NAME"));
+				typeArray.add(typeObject);
+			}
+		} else
+			resultObject.put("mess", "不存在商家");
+		resultObject.put("type", typeArray);
+		renderJson(resultObject);
+	}
+
+	public void addFoodType() {
+		String shopName = getPara("shopName");
+		String type = getPara("type");
+		Shop shop = foodService.findByShopName(shopName);
+		if (shop == null) {
+			renderJson("没找到商家啊~");
+			return;
+		}
+		if (foodService.existFoodType(shop.get("ID"), type)) {
+			renderJson(new JSONError("已存在分类╮(╯▽╰)╭"));
+			return;
+		}
+		FoodType foodType = new FoodType();
+		foodType.set("NAME", type);
+		foodType.set("SHOP_ID", shop.get("ID"));
+		foodType.save();
+		renderJson(new JSONSuccess("添加成功(｡◕ˇ∀ˇ◕)"));
 	}
 
 	public void checkFoodName() {
--- src/main/java/core/admin/service/food/FoodService.java
+++ src/main/java/core/admin/service/food/FoodService.java
@@ -1,13 +1,23 @@
 
 package core.admin.service.food;
 
+import java.util.List;
+
 import core.admin.service.base.DataTableService;
 import core.model.Food;
+import core.model.FoodType;
+import core.model.Shop;
 
 public interface FoodService extends DataTableService {
 	public Food findById(String id);
 
-	public boolean checkShopName(String shopName);
+	public Shop findByShopName(String shopName);
 
 	public boolean checkFoodName(String shopName);
+
+	public Food findByName(String name);
+
+	public List<FoodType> findFoodTypeByShopName(String shopName);
+
+	public boolean existFoodType(int shopId, String type);
 }
