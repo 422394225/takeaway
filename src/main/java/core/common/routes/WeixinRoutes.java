637909d6eed2fe8c1c5501adf22b3868d8ada8bd
WeiFaguo <422394225@qq.com>
2017-07-27 01:50:36 +0800

    店铺添加微信端一些修改

--- src/main/java/core/common/routes/WeixinRoutes.java
+++ src/main/java/core/common/routes/WeixinRoutes.java
@@ -10,6 +10,7 @@ import core.weixin.controller.WeixinMsgController;
 import core.weixin.controller.feedback.FeedbackController;
 import core.weixin.controller.shop.ShopController;
 import core.weixin.controller.test.TestController;
+import core.weixin.controller.tools.ToolsController;
 import core.weixin.controller.user.UserController;
 
 public class WeixinRoutes extends Routes {
@@ -26,6 +27,7 @@ public class WeixinRoutes extends Routes {
 		//controller路由
 		add("/wx/test", TestController.class, "/test");
 		add("/wx/shop", ShopController.class, "/shop");
+		add("/wx/tools", ToolsController.class, "/tools");
 
 		// api接口
 		add("/api/v1/user", UserController.class);
