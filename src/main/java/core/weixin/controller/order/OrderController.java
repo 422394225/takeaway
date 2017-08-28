0fa4b01ac9db4cbd0446688bed5e74c50ccbefec
jerry <jerry@192.168.0.103>
2017-08-29 02:36:52 +0800

    支付

--- src/main/java/core/weixin/controller/order/OrderController.java
+++ src/main/java/core/weixin/controller/order/OrderController.java
@@ -192,7 +192,6 @@ public class OrderController extends WeixinMsgController {
 			result.put("paySign", getSign(map));
 			renderJson(new JSONSuccess(result));
 		} else {
-			System.out.println(doc);
 			renderJson(new JSONError(doc.getElementsByTag("return_msg").text()));
 		}
 	}
